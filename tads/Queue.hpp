a7522a175179330bcceb77c5f8845c11815ea9a7
Zoe Mancini <zoemancini4@gmail.com>
2023-10-30 16:29:40 -0300

    Implementaciones del TAD Queue

--- tads/Queue.hpp
+++ tads/Queue.hpp
@@ -9,8 +9,11 @@ using namespace std;
 template<typename T>
 struct Queue
 {
+   Node<T>* q;
+   int size;
 };
 
+/*Crea una cola vacia y la retorna*/
 template<typename T>
 Queue<T> queue()
 {
@@ -18,29 +21,36 @@ Queue<T> queue()
    return q;
 }
 
+/*Encola el elemento e. Retorna la direccion de memoria del elemento que se encolo*/
 template<typename T>
 T* queueEnqueue(Queue<T>& q,T e)
 {
-   return NULL;
+   Node<T>* ret = enqueue<T>(q.q,e);
+   q.size++;
+   return &(ret->info);
 }
 
+/*Desencola un elemento y retorna el elemento que se desencolo*/
 template<typename T>
 T queueDequeue(Queue<T>& q)
 {
-   T t;
-   return t;
+   Node<T>* ret = dequeue<T>(q.q);
+   q.size--;
+   return ret->info;
 }
 
+/*Retorna true o false segun la cola tenga elementos o no*/
 template<typename T>
 bool queueIsEmpty(Queue<T> q)
 {
-   return true;
+   return isEmpty<T>(q.q);
 }
 
+/*Retorna la cantidad de elementos que tiene la cola*/
 template<typename T>
 int queueSize(Queue<T> q)
 {
-   return 0;
+   return q.size;
 }
 
 #endif
